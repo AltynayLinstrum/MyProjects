aef8866ba6aee7b643fee176494c9b7780b6b924
AltynayLinstrum <altynaylinstrum@yahoo.com>
2021-12-19 23:04:53 -0800

    Change 6

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const newYears = "1 Jan 2022";
 function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date ();
-
-    console.log(newYearsDate - currentDate);
 }
 countdown();
 
