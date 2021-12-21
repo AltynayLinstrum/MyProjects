d38cba90429756e7972d281d27aa268742178000
AltynayLinstrum <altynaylinstrum@yahoo.com>
2021-12-20 19:45:58 -0800

    Change 12

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-// const newYears = "1 Jan 2022";
+const newYears = "1 Jan 2022";
 
-// function countdown() {
-//     const newYearsDate = new Date(newYears);
-//     const currentDate = new Date ();
-// }
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date ();
+    
+    const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+
+    const minutes = Math.floor (totalSeconds / 60) % 60;
+
+    const seconds = Math.floor(totalSeconds) % 60;
+}
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);
